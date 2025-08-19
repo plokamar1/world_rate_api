a0642d39784009ff7fea68b6b8e256616b500fe0
Panagiotis Dorgiakis <ped@empist.com>
2025-08-19 15:45:22 +0300

    Initialize user with Country associations

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                   :bigint           not null, primary key
+#  reviews_count        :integer          default(0), not null
+#  username             :string           not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  nationality_id       :bigint
+#  residence_country_id :bigint
+#
+# Indexes
+#
+#  index_users_on_nationality_id        (nationality_id)
+#  index_users_on_residence_country_id  (residence_country_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (nationality_id => countries.id)
+#  fk_rails_...  (residence_country_id => countries.id)
+#
+class User < ApplicationRecord
+  belongs_to :nationality, class_name: "Country"
+  belongs_to :residence_country, class_name: "Country"
+end
