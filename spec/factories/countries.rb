bb8a076f4ddbdadbdf2f6afa211c9024f5403992
Panagiotis Dorgiakis <ped@empist.com>
2025-08-20 14:31:54 +0300

    Used faker data to populate country factory

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -2,20 +2,7 @@
 
 FactoryBot.define do
   factory :country do
-    name { "Greece" }
-    code { "GR" }
-    rating { 0 }
-    reviews_count { 0 }
-
-    # Traits for variations
-    # trait :admin do
-    #   role { 'admin' }
-    # end
-    #
-    # trait :with_reviews do
-    #   after(:create) do |user|
-    #     create_list(:review, 3, country: country)
-    #   end
-    # end
+    name { Faker::Address.country }
+    code { Faker::Address.country_code }
   end
 end
