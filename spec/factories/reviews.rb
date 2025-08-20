e2e84f72da78e8e279ebd12c8d831d62b25c5cf7
Panagiotis Dorgiakis <ped@empist.com>
2025-08-20 14:42:45 +0300

    generating text in review's description

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
   factory :review do
     country
     user
+    description { Faker::Lorem.paragraph(sentence_count: 3) }
     culture_rating { Random.rand(10) }
     expenses_rating { Random.rand(10) }
     food_rating { Random.rand(10) }
     nightlife_rating { Random.rand(10) }
     transportation_rating { Random.rand(10) }
-
   end
 end
