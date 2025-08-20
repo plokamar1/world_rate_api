2e1da4a8b95437fbf1f88e1e3dfc7e65460a522a
Panagiotis Dorgiakis <ped@empist.com>
2025-08-20 10:38:36 +0300

    Populating countries table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+CS.countries.each do |k,v|
+  Country.create!(name: v, code: k, calculated: true )
+end
