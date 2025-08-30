451fa1a8ca2d2218ce9babb992d69726f75ceaea
Panagiotis Dorgiakis <ped@empist.com>
2025-08-30 11:20:16 +0300

    Applying annotations

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: countries
+#
+#  id            :bigint           not null, primary key
+#  calculated    :boolean          default(TRUE), not null
+#  code          :string           not null
+#  name          :string           not null
+#  rating        :float            default(0.0), not null
+#  reviews_count :integer          default(0), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 FactoryBot.define do
   factory :country do
     name { Faker::Address.country }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                   :bigint           not null, primary key
+#  reviews_count        :integer          default(0), not null
+#  username             :string           not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  nationality_id       :bigint
+#  residence_country_id :bigint
+#
+# Indexes
+#
+#  index_users_on_nationality_id        (nationality_id)
+#  index_users_on_residence_country_id  (residence_country_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (nationality_id => countries.id)
+#  fk_rails_...  (residence_country_id => countries.id)
+#
 FactoryBot.define do
   factory :user do
     username { Faker::Internet.username(specifier: 5..10) }
