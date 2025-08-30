1d18e199b612d4f10b8b98a7ea823fdf008099c8
Panagiotis Dorgiakis <ped@empist.com>
2025-08-30 12:20:26 +0300

    Applying ransackable_attributes and ransackable_associations in all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.ransackable_attributes(auth_object = nil)
+    column_names.reject { |c| c.ends_with?("_id") }
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    reflect_on_all_associations.map(&:name)
+  end
 end
