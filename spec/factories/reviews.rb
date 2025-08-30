24ddb497fb44edc1ed26a3a35589ec8be9cfd6ce
Panagiotis Dorgiakis <ped@empist.com>
2025-08-30 11:19:02 +0300

    Tests for new validations and callbacks

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,14 +1,46 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                    :bigint           not null, primary key
+#  days_visited          :integer          not null
+#  description           :text             not null
+#  food_rating           :integer          default(0)
+#  nightlife_rating      :integer          default(0)
+#  safety_rating         :integer          default(0)
+#  total_expenses        :integer          default(100)
+#  total_rating          :float
+#  transportation_rating :integer          default(0)
+#  weight                :float
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  country_id            :bigint
+#  user_id               :bigint
+#
+# Indexes
+#
+#  index_reviews_on_country_id              (country_id)
+#  index_reviews_on_country_id_and_user_id  (country_id,user_id) UNIQUE
+#  index_reviews_on_user_id                 (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (country_id => countries.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
+  enum_values = [100, 500, 1000, 2000, 5000]
   factory :review do
     country
     user
+    days_visited { Random.rand(1..100) }
     description { Faker::Lorem.paragraph(sentence_count: 3) }
-    culture_rating { Random.rand(1..10) }
-    expenses_rating { Random.rand(1..10) }
+    total_expenses { enum_values[Random.rand(0..enum_values.size)] }
     food_rating { Random.rand(1..10) }
     nightlife_rating { Random.rand(1..10) }
     transportation_rating { Random.rand(1..10) }
+    safety_rating { Random.rand(1..10) }
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id                    :bigint           not null, primary key
+#  days_visited          :integer          not null
+#  description           :text             not null
+#  food_rating           :integer          default(0)
+#  nightlife_rating      :integer          default(0)
+#  safety_rating         :integer          default(0)
+#  total_expenses        :integer          default(100)
+#  total_rating          :float
+#  transportation_rating :integer          default(0)
+#  weight                :float
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  country_id            :bigint
+#  user_id               :bigint
+#
+# Indexes
+#
+#  index_reviews_on_country_id              (country_id)
+#  index_reviews_on_country_id_and_user_id  (country_id,user_id) UNIQUE
+#  index_reviews_on_user_id                 (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (country_id => countries.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 require 'shoulda-matchers'
 
@@ -16,12 +46,16 @@ RSpec.describe Review, type: :model do
     subject { build(:review) }
 
     it { should validate_length_of(:description).is_at_most(500) }
-    it { should validate_comparison_of(:culture_rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
-    it { should validate_comparison_of(:expenses_rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
+    it { should validate_presence_of(:description) }
+
     it { should validate_comparison_of(:food_rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
     it { should validate_comparison_of(:nightlife_rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
     it { should validate_comparison_of(:transportation_rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10).allow_nil }
+
+    it { should validate_inclusion_of(:total_expenses).in_array([100, 500, 1000, 2000, 5000]).allow_nil }
     it { should have_readonly_attribute(:total_rating) }
+    it { should have_readonly_attribute(:weight) }
+    it { should validate_comparison_of(:weight).is_greater_than(0).is_less_than_or_equal_to(1) }
     it { should validate_uniqueness_of(:user_id).scoped_to(:country_id) }
 
   end
@@ -39,11 +73,24 @@ RSpec.describe Review, type: :model do
       expect { create(:review, country: country) }.to change(country, :calculated)
     end
 
+    it "calculates weight before validation" do
+      country = create(:country)
+      review = create(:review, country: country,
+                      safety_rating: _5,
+                      food_rating: 5,
+                      nightlife_rating: 1,
+                      transportation_rating: 5,
+                      total_expenses: nil)
+
+      expect(review.weight).not_to be_nil
+      expect(review.weight).to be >= 0.1
+      expect(review.weight).to be < 1
+    end
+
     it "calculates total_rating before validation" do
       country = create(:country)
       review = create(:review, country: country,
-                      culture_rating: 5,
-                      expenses_rating: 1,
+                      safety_rating: _5,
                       food_rating: 5,
                       nightlife_rating: 1,
                       transportation_rating: 5)
