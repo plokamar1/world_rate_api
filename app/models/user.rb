d915da51f0f185d6b2a188dc395b7cacd58f5faf
Panagiotis Dorgiakis <ped@empist.com>
2025-08-30 11:30:43 +0300

    Added new attributes gender and email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 # Table name: users
 #
 #  id                   :bigint           not null, primary key
+#  email                :string           not null
+#  gender               :integer
 #  reviews_count        :integer          default(0), not null
 #  username             :string           not null
 #  created_at           :datetime         not null
--- db/migrate/20250819120200_create_users.rb
+++ db/migrate/20250819120200_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       t.string :username, null: false
+      t.string :email, null: false
+      t.integer :gender
       t.integer :reviews_count, default: 0, null: false
       t.belongs_to :nationality, foreign_key: { to_table: :countries }, index: true
       t.belongs_to :residence_country, foreign_key: { to_table: :countries }, index: true
