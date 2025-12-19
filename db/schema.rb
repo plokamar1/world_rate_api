c107d13a2c0811adf488aed1c419a3900bde2d87
plokamar1 <p.dorgiak@gmail.com>
2025-12-19 12:36:00 +0000

    commiting some base files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_120447) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_06_084211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.bigint "country_id", null: false
+    t.string "state", null: false
+    t.string "name", null: false
+    t.float "rating", default: 0.0, null: false
+    t.boolean "calculated", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id", "name"], name: "index_cities_on_country_id_and_name", unique: true
+    t.index ["country_id"], name: "index_cities_on_country_id"
+    t.index ["state"], name: "index_cities_on_state"
+  end
+
+  create_table "cities_reviews", force: :cascade do |t|
+    t.bigint "city_id", null: false
+    t.bigint "review_id", null: false
+    t.index ["city_id"], name: "index_cities_reviews_on_city_id"
+    t.index ["review_id"], name: "index_cities_reviews_on_review_id"
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "name", null: false
@@ -24,16 +44,34 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_120447) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.bigint "ratable_id", null: false
+    t.string "ratable_type", null: false
+    t.integer "food_rating", default: 0
+    t.integer "nightlife_rating", default: 0
+    t.integer "transportation_rating", default: 0
+    t.integer "safety_rating", default: 0
+    t.integer "nature_rating", default: 0
+    t.integer "sightseeing_rating", default: 0
+    t.float "total_rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.bigint "country_id"
     t.bigint "user_id"
-    t.text "description"
-    t.integer "food_rating", limit: 2, default: 0
-    t.integer "nightlife_rating", limit: 2, default: 0
-    t.integer "culture_rating", limit: 2, default: 0
-    t.integer "transportation_rating", limit: 2, default: 0
-    t.integer "expenses_rating", limit: 2, default: 0
+    t.text "description", null: false
+    t.integer "food_rating", default: 0
+    t.integer "nightlife_rating", default: 0
+    t.integer "transportation_rating", default: 0
+    t.integer "safety_rating", default: 0
+    t.integer "nature_rating", default: 0
+    t.integer "sightseeing_rating", default: 0
+    t.integer "days_visited", null: false
+    t.integer "total_expenses", default: 100
     t.float "total_rating"
+    t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["country_id", "user_id"], name: "index_reviews_on_country_id_and_user_id", unique: true
@@ -43,6 +81,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_120447) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
+    t.string "email", null: false
+    t.integer "gender"
     t.integer "reviews_count", default: 0, null: false
     t.bigint "nationality_id"
     t.bigint "residence_country_id"
